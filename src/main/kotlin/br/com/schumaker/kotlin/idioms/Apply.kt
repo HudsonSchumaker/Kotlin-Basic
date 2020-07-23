50437e4bbea5eac3da9b2120579c8d383f553930
Schumaker <hudson@schumaker.com.br>
2020-07-23 19:52:33 +0200

    Let & Apply

---
+++ src/main/kotlin/br/com/schumaker/kotlin/idioms/Apply.kt
@@ -0,0 +1,35 @@
+package br.com.schumaker.kotlin.idioms
+
+/**
+ * @author Hudson Schumaker
+ */
+object Apply {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        // Normal java way
+        val agentJavaWay = JamesBond()
+        agentJavaWay.name = "Sean Connery"
+        agentJavaWay.movie = "Satanic Dr. no"
+        agentJavaWay.alsoStarring = "The Rock"
+
+        println(agentJavaWay.printMe())
+
+        val `007` = JamesBond().apply {
+            name = "Sean Connery"
+            movie = "Satanic Dr. no"
+            alsoStarring = "From Russia with love"
+        }
+
+        println(`007`.printMe())
+    }
+}
+
+class JamesBond {
+    lateinit var name: String
+    lateinit var movie: String
+    lateinit var alsoStarring: String
+
+    fun printMe() {
+        print("[name = $name, movie = $movie, alsoStarring = $alsoStarring]")
+    }
+}
---
+++ src/main/kotlin/br/com/schumaker/kotlin/idioms/Let.kt
@@ -0,0 +1,16 @@
+package br.com.schumaker.kotlin.idioms
+
+import kotlin.random.Random
+
+/**
+ * @author Hudson Schumaker
+ */
+object Let {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sometimesNull = if (Random.nextBoolean()) "not null" else null
+        sometimesNull?.let {
+            print("It was $it this time")
+        }
+    }
+}

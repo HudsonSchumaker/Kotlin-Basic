42153740cc4a01fb80f1ca2cb49b8049f78588d2
Schumaker <hudson@schumaker.com.br>
2020-07-23 19:22:51 +0200

    Hello World

---
+++ src/main/kotlin/br/com/schumaker/kotlin/idioms/Main.kt
@@ -0,0 +1,12 @@
+package br.com.schumaker.kotlin.idioms
+
+/**
+ * @author Hudson Schumaker
+ */
+object Main {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("Hello World !!!")
+    }
+}

eb25eb76344299de099b72e920fee9a3d36daa6a
Schumaker <hudson@schumaker.com.br>
2020-07-23 20:38:40 +0200

    Instance check and smart cast

---
+++ src/main/kotlin/br/com/schumaker/kotlin/idioms/Instance.kt
@@ -0,0 +1,32 @@
+package br.com.schumaker.kotlin.idioms
+
+/**
+ * @author Hudson Schumaker
+ */
+object Instance {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+
+    fun whatInstance(superHero: SuperHero) {
+        when(superHero) {
+            is Batman -> superHero.callAlfred()
+            is Superman -> superHero.fly()
+            else -> println("Unknown superhero")
+        }
+    }
+}
+
+interface SuperHero
+class Batman : SuperHero {
+    fun callAlfred() {
+        println("To the Bat-cave")
+    }
+}
+class Superman : SuperHero {
+    fun fly() {
+        println("Up, up and away")
+    }
+}

d1632bb605d891c0f3e7c01ae5fad05e755029f1
Schumaker <hudson@schumaker.com.br>
2020-07-30 19:42:26 +0200

    Functions II

--- src/main/kotlin/br/com/schumaker/kotlin/functional/Functions.kt
+++ src/main/kotlin/br/com/schumaker/kotlin/functional/Functions.kt
@@ -6,12 +6,17 @@ package br.com.schumaker.kotlin.functional
 object Functions {
     @JvmStatic
     fun main(args: Array<String>) {
-        val myFunction: () -> Unit = ::test
+        //val myFunction: () -> Unit = ::test
+        val myFunction = ::test
         println(myFunction()) // See using the val as a method
+
+        val doSum: (a: Int, b: Int) -> Int = ::sum
+        println(doSum(2,2))
     }
 
     private fun test() {
         println("Putting a function on a variable")
     }
-}
 
+    private fun sum(a: Int, b: Int): Int = a + b
+}

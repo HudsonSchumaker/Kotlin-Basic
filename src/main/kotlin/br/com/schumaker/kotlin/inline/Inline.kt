402e7c136398a0af363e6f6e08ee503c1da4f6d4
Schumaker <hudson@schumaker.com.br>
2020-07-31 19:07:47 +0200

    Inline Functions

---
+++ src/main/kotlin/br/com/schumaker/kotlin/inline/Inline.kt
@@ -0,0 +1,26 @@
+package br.com.schumaker.kotlin.inline
+
+/**
+ * @author Hudson Schumaker
+ */
+object Inline {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("What am I doing ? ${doesNotMakeSense("I dummo")}")
+
+        makeMoreSense {
+            "here it is the block"
+        }
+    }
+
+    inline fun doesNotMakeSense(something: String) {
+        println(something)
+    }
+
+    inline fun makeMoreSense(block: () -> String) {
+        println("******")
+        println(block())
+        println("******")
+    }
+}

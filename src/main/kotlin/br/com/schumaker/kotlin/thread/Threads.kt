0461f59025dd270c4a3ab1cf8cedbae1dfc1f76e
Schumaker <hudson@schumaker.com.br>
2020-09-05 15:23:46 +0200

    Threads

---
+++ src/main/kotlin/br/com/schumaker/kotlin/thread/Threads.kt
@@ -0,0 +1,17 @@
+package br.com.schumaker.kotlin.thread
+
+import kotlin.concurrent.thread
+
+/**
+ * @author Hudson Schumaker
+ */
+object Threads {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val t1 = thread {
+            for (i in 1..100) {
+                println("T1: $i");
+            }
+        }
+    }
+}

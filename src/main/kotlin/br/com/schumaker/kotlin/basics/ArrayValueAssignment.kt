8ad7525ed434546f451a84a279ead6b9f1425d32
Schumaker <hudson@schumaker.com.br>
2020-09-12 10:59:26 +0200

    Basics

---
+++ src/main/kotlin/br/com/schumaker/kotlin/basics/ArrayValueAssignment.kt
@@ -0,0 +1,8 @@
+package br.com.schumaker.kotlin.basics
+
+class ArrayValueAssignment {
+    val numbers = arrayOf(1, 2, 3, 4)
+    println(numbers[0]) // Prints 1
+    numbers[0] = 23
+    println(numbers[0]) // Prints 23
+}

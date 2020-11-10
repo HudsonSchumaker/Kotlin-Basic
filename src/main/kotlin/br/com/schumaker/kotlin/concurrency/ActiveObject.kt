5f704996a439d44203b0276be6512025c9836244
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-11-10 19:08:34 +0100

    Create ActiveObject.kt

---
+++ src/main/kotlin/br/com/schumaker/kotlin/concurrency/ActiveObject.kt
@@ -0,0 +1,25 @@
+fun activeActor(out: SendChannel<String>) = actor<Int> {
+  for (i in this) {
+     out.send(i.toString().reversed())
+  }
+  out.close()
+}
+
+val channel = Channel<String>()
+val actor = activeActor(channel)
+
+val j1 = launch {
+  for (i in 42..53) {
+    actor.send(i)
+  }
+  actor.close()
+}
+
+val j2 = launch {
+  for (in inchannel {
+    print(i)
+  }
+}
+
+j1.join()
+j2.join()

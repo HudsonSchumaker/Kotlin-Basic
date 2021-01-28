a3db698f301e5a96934bec1e16aa2298c978809f
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-28 09:11:39 +0100

    Update Class1.kt

--- src/main/kotlin/br/com/schumaker/kotlin/class/Class1.kt
+++ src/main/kotlin/br/com/schumaker/kotlin/class/Class1.kt
@@ -1,3 +1,6 @@
+/**
+ * @author Hudson Schumaker
+ */
 class Person(val name: String) {
     var children: MutableList<Person> = mutableListOf()
     constructor(name: String, parent: Person) : this(name) {

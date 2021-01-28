89e1f320f59fd5889311ce133e2b48b0cb6db5bc
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-28 08:55:09 +0100

    Update When.kt

--- src/main/kotlin/br/com/schumaker/kotlin/idioms/When.kt
+++ src/main/kotlin/br/com/schumaker/kotlin/idioms/When.kt
@@ -1,7 +1,14 @@
-when (x) {
-    1 -> print("x == 1")
-    2 -> print("x == 2")
-    else -> { // Note the block
-        print("x is neither 1 nor 2")
+package br.com.schumaker.kotlin.idioms
+
+/**
+ * @author Hudson Schumaker
+ */
+object When {
+    when (x) {
+        1 -> print("x == 1")
+        2 -> print("x == 2")
+        else -> { // Note the block
+            print("x is neither 1 nor 2")
+        }
     }
 }

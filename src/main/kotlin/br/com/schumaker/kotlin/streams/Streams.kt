e2aa3897a3f5e4eccdffc546d8d0b4eb51d5f6d1
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-01-30 16:39:05 +0100

    Create Streams.kt

---
+++ src/main/kotlin/br/com/schumaker/kotlin/streams/Streams.kt
@@ -0,0 +1,9 @@
+
+class Streams {
+    val numbers = (1..10).toList()
+
+    // Return a view, no copy here
+    val reversed = (1..10).toList().asReversed()
+
+    val sequence = (1..10).toList().asSequence()
+}

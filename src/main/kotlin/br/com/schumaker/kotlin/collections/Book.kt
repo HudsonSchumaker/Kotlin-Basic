367f55ed76c3bc6ccb2816aefe81d80ef372e6d3
Hudson Schumaker <hudson.schumaker@me.com>
2021-07-17 16:30:57 +0200

    changes

---
+++ src/main/kotlin/br/com/schumaker/kotlin/collections/Book.kt
@@ -0,0 +1,12 @@
+package br.com.schumaker.kotlin.collections
+
+data class Book (
+    val title: String,
+    val author: String,
+    val year: Long,
+    val press: String? = null
+): Comparable<Book> {
+    override fun compareTo(other: Book): Int {
+        return this.year.compareTo(other.year)
+    }
+}

825248f5a39c2a9b40e76fd8f2ac96c811a897c7
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-11-06 19:53:53 +0100

    Update Main.kt

--- src/main/kotlin/br/com/schumaker/kotlin/basics/Main.kt
+++ src/main/kotlin/br/com/schumaker/kotlin/basics/Main.kt
@@ -1,5 +1,9 @@
-
+package br.com.schumaker.kotlin.basics
 
 fun main() {                        
     println("Hello, World!")        
+}
+
+fun main(args: Array<String>) {
+    println("Hello, World!")
 }
